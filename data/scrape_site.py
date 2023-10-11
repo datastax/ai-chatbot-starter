c9289af18e1acf1d8b39737ea9a3726e480423fd
Eric Hare <ericrhare@gmail.com>
2023-10-11 14:56:58 -0700

    Add fixed scraping routine

--- data/scrape_site.py
+++ data/scrape_site.py
@@ -5,12 +5,12 @@ sys.path.append("../")
 
 from dotenv import load_dotenv
 
-load_dotenv(".env")
+load_dotenv("../.env")
 
 
 # The below line will be red in Pycharm, but it's fine
 from chatbot_api.crawl_scrape_docs import crawl_website_parallel
 
 # Astra docs
-for website in os.getenv("CUSTOM_RULES").split("\n"):
+for website in os.getenv("DOCUMENTATION_PAGES").split(","):
     crawl_website_parallel(website, f"docs/{website}.txt")
