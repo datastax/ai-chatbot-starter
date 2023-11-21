19d84d2f374852ddbeec7033f7b8a7cc1573d724
Eric Hare <ericrhare@gmail.com>
2023-11-21 09:53:52 -0800

    Update scrape_site.py

--- data/scrape_site.py
+++ data/scrape_site.py
@@ -1,17 +1,14 @@
 import os
-import sys
 
 from urllib.parse import urlparse
-
-sys.path.append("../")
 
 from dotenv import load_dotenv
 
 from chatbot_api.crawl_scrape_docs import crawl_website_parallel
 from pipeline.config import load_config
 
-load_dotenv("../.env")
-config = load_config("../config.yml")
+load_dotenv(".env")
+config = load_config("config.yml")
 
 # Astra docs
 for website in config.doc_pages:
